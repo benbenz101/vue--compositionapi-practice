47d543d64633e12f93ea6f284f8b9cbeaaaf1567
benbenz101 <bensonbenzene@gmail.com>
2022-04-19 09:56:13 +0100

    learning to use template refs in the setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>my name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick" >click me</button>
+    <p ref="p">my name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "Home",
   //the setup will run before any of the lifecycle hooks
@@ -17,16 +18,28 @@ export default {
 
     //this alone isnt reactive like the options api
     //if the values changes here, it won't reflect on the template
+    //we can use template ref to store references in our template  normallyin vue, but in composition-api, it is used to make data elements reactive
+    //the 'this' keyword isn't available in the setup function
+    //in composition api, we import the part that we need from vue
+    //
+
+    const p = ref(null);
+
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      //we can take the value and do any JS operation on it
+      p.value.classList.add("test");
+      p.value.textContent = "welcome Benny";
     };
 
     //always return anything you have in the setup to make them available in the template
+    //If we don't return the p for example, it won't be associated with the p in the dorm element
+    //
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
